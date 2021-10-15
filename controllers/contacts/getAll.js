661cc8714e5ef2aba4a1d22475ace2e67dbeeb33
Puziichuk Olena <nonna467@gmail.com>
2021-10-15 17:00:45 +0300

    added hw05-avatars

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -3,8 +3,10 @@ const { sendSuccessRes } = require('../../helpers')
 const { Contact } = require('../../models')
 
 const getAll = async (req, res) => {
+  const { page, limit } = req.query
+  const skip = (page - 1) * limit
   const userId = req.user.id
-  const result = await Contact.find({ owner: userId }, '_id name email phone favorite')
+  const result = await Contact.find({ owner: userId }, '_id name email phone favorite', { skip, limit: +limit })
   sendSuccessRes(res, { result })
 }
 
